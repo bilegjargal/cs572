dc286567c06110985379c81448cc0ee81bd316e0
angarag <mars0411@gmail.com>
2019-01-14 21:18:10 -0600

    Rxjs related stuffs added

--- homework/assignment_1_D.js
+++ homework/assignment_1_D.js
@@ -1,12 +1,14 @@
-(function() {
-    String.prototype.filterWords = function(arr) {
-      let text = this.toString();
-
-      return text;
+(function () {
+    const rxjs = require('rxjs');
+    const { from } = rxjs;
+    //const {map, filter} = rxjs.operators;
+    String.prototype.filterWords = function (arr) {
+        let text = this.toString();
+        from(text).subscribe(i => { console.log(i); });
+        return text;
     };
-  
+
     const test = "This house is nice";
     const bannedWords = ["house", "nice"];
     console.log(test.filterWords(bannedWords));
-  })();
-  
+})();
