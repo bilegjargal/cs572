f84ac89b0d5383d4ab1ff45aa4dd15b20b25f11d
angarag <mars0411@gmail.com>
2019-01-14 23:24:30 -0600

    HW1_4 done with Observables

--- homework/assignment_1_D.js
+++ homework/assignment_1_D.js
@@ -1,14 +1,35 @@
 (function () {
     const rxjs = require('rxjs');
-    const { from } = rxjs;
-    //const {map, filter} = rxjs.operators;
+    const { of, from } = rxjs;
+    const { map, filter, mergeMap } = require('rxjs/operators');//rxjs.operators;
     String.prototype.filterWords = function (arr) {
         let text = this.toString();
-        from(text).subscribe(i => { console.log(i); });
-        return text;
+        let new_text = [];
+        //if(arr.includes('This'))
+        const source = from(text.split(' '));
+        const emits = source.pipe(
+            map(
+                (val) => {
+                    if (arr.includes(val) == true) {
+                        val = '***';
+                    }
+                    return val;
+
+                }));
+        const subscribtion = emits.subscribe(
+            i => {
+                new_text.push(i);
+                //    console.log(i);
+            },
+            null,
+            i => { //console.log('done'); 
+            }
+        );
+        return new_text.join(' ').toString();
     };
 
     const test = "This house is nice";
     const bannedWords = ["house", "nice"];
-    console.log(test.filterWords(bannedWords));
+    if (test.includes(bannedWords[0]))
+        console.log(test.filterWords(bannedWords));
 })();
