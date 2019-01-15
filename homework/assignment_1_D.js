a1c03b6436428e777aa70299923f59469f8906af
angarag <mars0411@gmail.com>
2019-01-14 23:32:08 -0600

    HW1_4 corrected

--- homework/assignment_1_D.js
+++ homework/assignment_1_D.js
@@ -5,12 +5,11 @@
     String.prototype.filterWords = function (arr) {
         let text = this.toString();
         let new_text = [];
-        //if(arr.includes('This'))
         const source = from(text.split(' '));
         const emits = source.pipe(
             map(
                 (val) => {
-                    if (arr.includes(val) == true) {
+                    if (arr.includes(val) === true) {
                         val = '***';
                     }
                     return val;
@@ -19,10 +18,11 @@
         const subscribtion = emits.subscribe(
             i => {
                 new_text.push(i);
-                //    console.log(i);
             },
-            null,
-            i => { //console.log('done'); 
+            i => { console.log('error occured'); },
+            i => {
+                new_text.push(i);
+                //  console.log('done'); 
             }
         );
         return new_text.join(' ').toString();
@@ -30,6 +30,5 @@
 
     const test = "This house is nice";
     const bannedWords = ["house", "nice"];
-    if (test.includes(bannedWords[0]))
-        console.log(test.filterWords(bannedWords));
+    console.log(test.filterWords(bannedWords));
 })();
