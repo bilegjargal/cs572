dd27ffa7444a0fcf2bfa0becd82ab8e4d354a749
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-01-18 21:29:14 -0600

    homework 3 fixed

--- homework_3/assignment_3.js
+++ homework_3/assignment_3.js
@@ -8,23 +8,22 @@ const mimeType = {
 };
 
 server.on("request", function(req, res) {
+  const useage = process.memoryUsage();
   res.writeHead(200, { "Content-Type": mimeType[".mkv"] });
 
   fs.readFile("./demo.mkv", function(err, content) {
-    console.log(content.length);
+    console.log(useage);
   });
 
-  // fs.readFileSync("./demo.mkv", function(err, content) {
-  //   console.log(content.mimeType);
-  // });
+  fs.readFileSync("./demo.mkv", function(err, content) {
+    console.log(useage);
+  });
 
-  // process.chdir("./");
-
-  // var stream = fs.createReadStream("./demo.mkv", { bufferSize: 64 * 1024 });
-  // stream.pipe(res);
-  // res.end();
+  process.chdir("./");
+  var stream = fs.createReadStream("./demo.mkv", { bufferSize: 64 * 1024 });
+  stream.pipe(res);
+  res.end();
 });
 
 server.listen(4000);
-
 console.log("Server has started");
