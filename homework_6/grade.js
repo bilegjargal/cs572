27b46d42a0655e8954c31f03348a419a53a6d26b
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-01-20 17:09:39 -0600

    homework 6 not finished

--- homework_6/grade.js
+++ homework_6/grade.js
@@ -1,11 +1,44 @@
 class Grade {
-  constructor(id, name, course, grade) {
-    this.id = id;
-    this.name = name;
-    this.course = course;
-    this.grade = grade;
+  constructor() {
+    this.grades = [];
+    this.grades.push(
+      {
+        id: 1,
+        name: "Asaad Saad",
+        course: "CS572",
+        grade: 97
+      },
+      {
+        id: 2,
+        name: "Bilegjargal",
+        course: "CS572",
+        grade: 95
+      }
+    );
+  }
+
+  getAll() {
+    return this.grades;
+  }
+
+  getGradeById(id) {
+    return this.grades.filter(this.grades.id == id);
+  }
+
+  addGrade(grade) {
+    this.grades.push(grade);
+    console.log("Grade added");
+  }
+
+  removeGrade(id) {
+    for (let i = 0; i < this.grades.length; i++) {
+      if (this.grades[i].id == id) {
+        this.grades.splice(i, 1);
+        return console.log("Grade deleted");
+      }
+    }
+    return console.log("Not found");
   }
 }
 
-const grade = new Grade(1, "Bilegjargal", "CS572", 97);
-console.log(grade);
+module.exports = new Grade();
--- homework_6/server.js
+++ homework_6/server.js
@@ -1,18 +1,77 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const logger = require("morgan");
 const grades = require("./grade");
+const { check, validationResult } = require("express-validator/check");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set("env", "development");
 app.enable("case sensitive routing");
 app.set("x-powered-by", false);
 
-app.get("/grades", function(req, res, next) {
-  res.send(grades);
+app.get("/grades", function(req, res) {
+  res.send(grades.getAll());
+  next();
 });
 
-app.post("/grades", function(req, res, next) {
-  res.send(grades);
+app.post(
+  "/add",
+  [
+    check("id").isNumeric(),
+    check("name").exists(),
+    check("course").exists(),
+    check("grade").isNumeric()
+  ],
+  function(req, res) {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    grades.addGrade({
+      id: req.body.id,
+      name: req.body.name,
+      course: req.body.course,
+      grade: req.body.grade
+    });
+    console.log(req.body);
+  }
+);
+
+app.post(
+  "/add",
+  [
+    check("id").isNumeric(),
+    check("name").exists(),
+    check("course").exists(),
+    check("grade").isNumeric()
+  ],
+  function(req, res) {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    grades.addGrade({
+      id: req.body.id,
+      name: req.body.name,
+      course: req.body.course,
+      grade: req.body.grade
+    });
+    console.log(req.body);
+  }
+);
+
+app.delete("/remove/:id", function(req, res) {
+  console.log(req.params.id);
+  grades.removeGrade(req.params.id);
+  next();
 });
 
 app.listen(4000, () => console.log("Listening on 4000"));
