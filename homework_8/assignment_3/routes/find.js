7103d485e7875e29b92ffb8ff3da3d4c385d19d7
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-01-23 09:47:47 -0600

    [FIX] homework8 assignment3

--- homework_8/assignment_3/routes/find.js
+++ homework_8/assignment_3/routes/find.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const url_obj = require("url");
 
 router.get("/", (req, res) => {
   const collection = req.db.collection("homework8");
-  collection.createIndex({ location: "2d" });
+  const params = url_obj.parse(req.url, true);
+  // collection.createIndex({ "2dsphere": "2d" });
+
   collection
-    .find(
-      { location: { $near: [req.params.long, req.params.lat] } },
-      { _id: 0, name: 1, category: 1, location: 1 }
-    )
-    .limit(3);
+    .find({
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [
+              parseFloat(params.query.long),
+              parseFloat(params.query.lat)
+            ]
+          },
+          $maxDistance: 5000,
+          $minDistance: 1000
+        }
+      }
+    })
+    .limit(3)
+    .toArray(function(err, document) {
+      if (err) throw err;
+      res.status(200).json(document);
+    });
   res.end();
 });
 
